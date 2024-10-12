70d829e601b7aa46595aac44388f9762b3944575
Milena153 <DukeSachenko14@gmail.com>
2024-10-12 17:00:03 +0300

    add hometask

---
+++ урок 6/practice-hw/script.js
@@ -0,0 +1,19 @@
+//задание 1 
+function min(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+
+//задание 2
+let result =x;
+function pow(x,n){
+    if (x*n){
+        return x;
+    } else {
+        return n;
+    }
+
+}
