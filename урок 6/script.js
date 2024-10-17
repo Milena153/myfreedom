a52c82f73bc47904a9f07164c9a7b988fe8adefa
Milena153 <DukeSachenko14@gmail.com>
2024-10-17 17:26:00 +0300

    aaa

---
+++ урок 6/script.js
@@ -0,0 +1,31 @@
+//ЗАДАНИЕ 1
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+//   ask(
+//     "Вы согласны?",
+//     () => { alert("Вы согласились."); }, // анонимная функция
+//     () => { alert("Вы отменили выполнение."); }
+//   );
+  
+//ЗАДАНИЕ 2
+// function greet(name,calback) {
+//   console.log(`hello,${name}!`);
+//   calback();
+// }
+// function goodbye(){
+//     console.log('Good Bye!')
+// }
+// greet('alina', goodbye)
+
+//задание 3
+// function doubleNumber(number,calback){
+//     const num = number *2;
+//     calback(num);
+// }
+// function doubleNumberr(doubleNumber){
+//     console.log(doubleNumber);
+// };
+// doubleNumber(2,doubleNumberr)
